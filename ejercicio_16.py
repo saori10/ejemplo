74b82c41420e82e6466677c4323756eefcf32070
saori10 <saori.rg09@gmail.com>
2024-01-15 19:55:37 -0500

    remove comments

--- ejercicio_16.py
+++ ejercicio_16.py
@@ -1,12 +1,11 @@
 
-"""
 phone = input("Introduce un numero de telefono con el formato +xx-xxxxxxxxx-xx: ")
 phoneList = phone.split('-')
 number = phoneList[1]
 print(number)
 
-numbers = phone.split('-')[1]
-print(numbers)
-"""
+#numbers = phone.split('-')[1]
+#print(numbers)
 
-print("Hola"[::-1])
+
+#print("Hola"[::-1])

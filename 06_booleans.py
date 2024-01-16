ecd13d54a4b748069c95c126b410f1fc20f5f8c5
saori10 <saori.rg09@gmail.com>
2024-01-15 21:02:02 -0500

    add class 6

---
+++ 06_booleans.py
@@ -0,0 +1,11 @@
+is_single = True
+print(type(is_single))
+is_single = False
+print(is_single)
+
+# not invierte el valor de un booleano
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)

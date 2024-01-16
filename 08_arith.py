bfdcc5b73a325dbc9e7f300b1eea942e794ec1e0
saori10 <saori.rg09@gmail.com>
2024-01-15 21:04:42 -0500

    add class 8

---
+++ 08_arith.py
@@ -0,0 +1,24 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) # 5
+print(10 % 2) # residuo
+print(10 / 3)
+print(10 % 3)
+print(10 // 3) # solo el numero entero
+print(2 ** 3) # la potenciacion
+print(2 ** 3 + 3 - 7 / 1 // 4) 
+
+# PRIORIDAD Y ORDEN EN EL QUE SE RESUELVEN LAS OPERACIONES ARITMETICAS
+   # P = parentesis
+   # E = exponentes
+   # M = multiplicacion
+   # D = division
+   # A = adicion
+   # S = sustraccion
+
+# VA DE IZQUIERDA A DERECHA
+
+
+print("Hola" + " mundo")
+print("Hola " * 3)

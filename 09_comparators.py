37fb0d64563da726b776d8a47b9be4992807dc88
saori10 <saori.rg09@gmail.com>
2024-01-15 21:06:05 -0500

    add class 9

---
+++ 09_comparators.py
@@ -0,0 +1,36 @@
+# > Mayor que
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+# < Menor que
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+
+# >= Mayor o igual que
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+
+# <= Menor o igual que
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+
+# == Igual que
+print(6 == 6)
+print(5 == 2)
+
+# != Diferente que
+print(6 != 10)
+print(6 != 6)
+
+
+
+print("Apple" == "Apple")
+print("Apple" == "apple")
+print("1" == 1)
+
+age = 15
+print(age >= 18)

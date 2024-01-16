bde2f68af032ce54bcbb2fb13ed32525841859f4
saori10 <saori.rg09@gmail.com>
2024-01-15 20:55:45 -0500

    add class 2

---
+++ 02_vars.py
@@ -0,0 +1,18 @@
+print("hola, vars")
+
+my_name = "Saori"
+print(my_name)
+
+my_age = 14
+print(my_age)
+
+my_name = "Snaider"
+print("aqui cambio :", my_name)
+
+# se hara uso del input para ingresar un valor
+
+my_name = input("¿Cual es tu nombre?")
+print("usando input", my_name)
+
+my_age = input("¿Cual es tu edad?")
+print("utilizando input", my_age)

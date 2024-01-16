45ab24e957c5db0e68eb145eeeea759837868294
saori10 <saori.rg09@gmail.com>
2024-01-15 21:03:52 -0500

    add class 7

---
+++ 07_transformation.py
@@ -0,0 +1,21 @@
+name = "Saori"
+print(type(name))
+name = 14
+print(type(name))
+name = True
+print(type(name))
+
+print("Saori " + "Rocillo")
+print(14 + 6)
+
+# Para que la operacion de resultado todas deben de ser del mismo tipo 
+
+age = 14
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input("Escribe tu edad => ")
+print(type(age))
+age = int(age)
+age += 10
+print(f"Tu edad en 10 años sera {age}")

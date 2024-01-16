15472c386b16a8649427bd75e3f6bdb52e32ff60
saori10 <saori.rg09@gmail.com>
2024-01-15 20:57:40 -0500

    add class 3

---
+++ 03_types.py
@@ -0,0 +1,21 @@
+# string = cadena de texto
+my_name = "saori"
+print("my_name =>", my_name)
+
+# para saber el tipo de dato se usa type :
+print(type(my_name))
+
+# int = enteros o numeros
+my_age = 14
+print("my_age =>", my_age)
+print(type(my_age))
+
+# boolean = True or False
+is_single = False
+print("is_single =>", is_single)
+print(type(is_single))
+
+# inputs
+my_age = input("¿Cuál es tu edad?")
+print("my_age =>", my_age)
+print(type(my_age))

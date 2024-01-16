2deb669e90c7728d25e27f73bb64591bd736871f
saori10 <saori.rg09@gmail.com>
2024-01-15 21:00:54 -0500

    add class 5

---
+++ 05_numbers.py
@@ -0,0 +1,29 @@
+lives = 3
+print(type(lives))
+
+age = 12
+budget = 100
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 25 + 15
+print(lives)
+
+lives = lives - 5
+print(lives)
+
+# forma abreviada se puede poner suma y resta
+lives -= 1
+print(lives)
+
+number = 4500000000000000000.1
+print(number)
+
+number_b = 0.000000000000000001
+print(number_b)

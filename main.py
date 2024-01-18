b99289362f0080eda756be3c5d2affe7a6aef03a
saori10 <saori.rg09@gmail.com>
2024-01-18 18:36:00 -0500

    add the game of rock, paper, scissors

---
+++ main.py
@@ -0,0 +1,26 @@
+user_option = input("Elige, piedra, papel o tijera: ")
+computer_option = "piedra"
+
+if user_option == computer_option:
+    print("Empate!")
+elif user_option == "piedra":
+    if computer_option == "tijera":
+        print("piedra gana a tijera")
+        print("user ganó!")
+    else:
+        print("papel gana a piedra") 
+        print("computer ganó!")
+elif user_option == "papel":
+    if computer_option == "piedra":
+        print("papel gana a piedra")
+        print("user ganó!")
+    else:
+        print("tijera gana a papel") 
+        print("computer ganó! ")
+elif user_option == "tijera":
+    if computer_option == "papel":
+        print("tijera gana a papel")
+        print("user ganó!")
+    else:
+        print("piedra gana a tijera")
+        print("computer ganó!")                
